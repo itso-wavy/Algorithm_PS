892ff0d9a6f9354f87cab4abd628d59513a578b8
wavy_ <you.meet.vvavy@gmail.com>
2022-11-15 12:19:49 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,15 +1,3 @@
-// 풀이1
-function solution(angle) {
-    if(angle < 90) {
-        return 1
-    } else if(angle == 90) {
-        return 2
-    } else if(angle < 180) {
-        return 3
-    } else return 4
-}
-
-// 풀이2
 function solution(angle) {
     return [90, 91, 180, 181].findIndex(i => i > angle) + 1
 }
