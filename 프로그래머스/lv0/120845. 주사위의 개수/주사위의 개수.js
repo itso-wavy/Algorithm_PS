201afd1bdbf4edcbc78f67e2aaa02aa787c2177f
wavy_ <you.meet.vvavy@gmail.com>
2023-03-26 22:24:30 +0900

    [level 0] Title: 주사위의 개수, Time: 0.06 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,6 +1,3 @@
 function solution(box, n) {
-    const a = ~~(box[0] / n)
-    const b = ~~(box[1] / n)
-    const c = ~~(box[2] / n)
-    return a*b*c
+    return box.reduce((a,v) => ~~(v / n) * a, 1)
 }
