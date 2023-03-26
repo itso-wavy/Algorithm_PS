b6eb1d912f0bb8a7311905d15b655abfe36e8b50
wavy_ <you.meet.vvavy@gmail.com>
2023-03-27 02:27:55 +0900

    [level 0] Title: 약수 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,6 @@
 function solution(n) {
     const arr = [];
-    let i = 0;
-    while(i <= n){
-        i++;
+    for(let i = 0; i <= n; i++)
         n % i === 0 ? arr.push(i) : undefined;      
-    }
     return arr;
 }
