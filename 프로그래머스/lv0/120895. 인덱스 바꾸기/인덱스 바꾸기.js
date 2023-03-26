8a7a8f8fc66c5271cf9c334daefca1a99081d6cd
wavy_ <you.meet.vvavy@gmail.com>
2023-03-27 00:43:53 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,6 +1,12 @@
 function solution(my_string, num1, num2) {
-    const new_string = [...my_string]
-    new_string[num2] = my_string[num1]
-    new_string[num1] = my_string[num2]
-    return new_string.join('');
+    // 1)
+    // const new_string = [...my_string]
+    // new_string[num2] = my_string[num1]
+    // new_string[num1] = my_string[num2]
+    // return new_string.join('');
+    
+    // 2)
+    my_string = my_string.split('');
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    return my_string.join('');
 }
