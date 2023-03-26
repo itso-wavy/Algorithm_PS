3852f990e28f17d70885b2099f7bad4af17436e2
wavy_ <you.meet.vvavy@gmail.com>
2023-03-27 01:00:41 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,10 @@
+function solution(numbers, direction) {
+    if(direction === "right") {
+        numbers.unshift(numbers.at(-1));
+        numbers.pop();
+    } else {
+        numbers.push(numbers[0]);
+        numbers.shift();
+        }
+    return numbers;
+}
