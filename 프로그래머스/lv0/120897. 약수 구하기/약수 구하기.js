af43f30ece58343b08860149fbc39502ba86c429
wavy_ <you.meet.vvavy@gmail.com>
2023-03-27 02:39:40 +0900

    Update 약수 구하기.js

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,6 +1,23 @@
 function solution(n) {
-    const arr = [];
-    for(let i = 0; i <= n; i++)
-        n % i === 0 ? arr.push(i) : undefined;      
-    return arr;
+    // 1)
+    // const arr = [];
+    // for(let i = 0; i <= n; i++)
+    //     n % i === 0 ? arr.push(i) : undefined;      
+    // return arr;
+    
+    // 2)
+    // return Array(n).fill(0).map((v, i) => v+i+1).filter(i => n % i === 0)
+    
+    // 3)
+    // return Array.from({ length: n }, (_, i) => i + 1).filter(i => n % i === 0)
+    
+    // 4)
+    let s = new Set();
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            s.add(i);
+            s.add(n / i);
+        }
+    }
+    return [...s].sort((a, b) => a - b);
 }
