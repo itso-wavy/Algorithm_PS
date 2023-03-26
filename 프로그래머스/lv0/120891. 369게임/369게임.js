f043f28ada7aff8f5881d6b360b5931cc417b25a
wavy_ <you.meet.vvavy@gmail.com>
2023-03-27 02:15:02 +0900

    Update 369게임.js

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,3 +1,11 @@
 function solution(order) {
-    return [...order.toString().matchAll(/[369]/g)].length
+    // 1)
+    let counter = 0;
+    [...order.toString()].forEach(i => {
+        if(i == 3||i == 6||i == 9)
+            counter++
+    })
+    return counter;
+    // 2)
+    // return ('' + order).split(/[369]/).length - 1
 }
