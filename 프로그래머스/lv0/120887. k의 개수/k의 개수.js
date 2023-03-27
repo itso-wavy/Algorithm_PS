3cebfcc4dd8fc9327d523995af694adb9923dbf1
wavy_ <you.meet.vvavy@gmail.com>
2023-03-28 04:50:39 +0900

    [level 0] Title: k의 개수, Time: 5.76 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,3 +1,5 @@
 function solution(i, j, k) {
-    return Array.from({length: j - i + 1}, (v, index) => index + i).join('').split(k).length - 1
+    let str = ''
+    for(i; i <= j; i++) str = str + i
+    return str.split(k).length - 1
 }
