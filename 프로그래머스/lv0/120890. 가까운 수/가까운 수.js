a4d978793e8642ccfc41e28808362f5f2ff1b42a
wavy_ <you.meet.vvavy@gmail.com>
2023-03-28 04:33:22 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,4 +1,9 @@
 function solution(array, n) {
-    const sub = array.reduce((a, v) => Math.min(Math.abs(v - n), a), Math.abs(array[0] - n))
-    return array.includes(n - sub) ? n - sub : n + sub
+    // 1)
+    // const sub = array.reduce((a, v) => Math.min(Math.abs(v - n), a), Math.abs(array[0] - n))
+    // return array.includes(n - sub) ? n - sub : n + sub
+    
+    // 2)
+    array.sort((a,b) => Math.abs(n - a) - Math.abs(n - b) || a - b)
+    return array[0]
 }
