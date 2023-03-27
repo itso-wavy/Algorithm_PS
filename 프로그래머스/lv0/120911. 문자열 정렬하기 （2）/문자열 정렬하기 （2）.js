e9bb7c24d8c7974112cdc51d81e1357e9595790f
wavy_ <you.meet.vvavy@gmail.com>
2023-03-28 00:34:31 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,10 +1,3 @@
 function solution(my_string) {
-    let arr = []
-    let arr2 = []
-    for(el of my_string.toLowerCase()) {
-        arr.push('abcdefghijklmnopqrstuvwxyz'.indexOf(el))
-        arr = arr.sort((a, b) => a - b)
-    }
-    arr.forEach(i => arr2.push('abcdefghijklmnopqrstuvwxyz'[i]))
-    return arr2.join('');
+    return [...my_string.toLowerCase()].sort().join('')
 }
