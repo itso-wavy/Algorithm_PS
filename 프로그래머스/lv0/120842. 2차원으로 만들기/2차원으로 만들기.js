9c0a45a79fdb8015688b0290a5e171599c3edfe6
wavy_ <you.meet.vvavy@gmail.com>
2023-03-31 01:37:33 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,11 +1,5 @@
 function solution(num_list, n) {
     const arr = []
-    for(let i = 0; i < num_list.length; i = i + n) {
-        const arr2 = []
-        for(let j = i; j < i + n; j++) {
-            arr2.push(num_list[j])
-        }
-        arr.push(arr2)
-    }
+    while(num_list.length) arr.push(num_list.splice(0, n))
     return arr
 }
