adcbe88be5eb47f79548706d7b8ff5bab2a480a7
wavy_ <you.meet.vvavy@gmail.com>
2023-04-02 03:03:33 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,8 +1,3 @@
 function solution(my_str, n) {
-    var answer = [];
-    for(let i = 0; i < my_str.length; i = i+n) {
-        answer.push(my_str.slice(i, i+n))
-    }
-    return answer;
-    // return my_str.match(new RegExp(`.{1,${n}}`, "g"));
+    return my_str.match(new RegExp(`.{1,${n}}`, 'g'))
 }
