02a5b45f6b5c4b0412e74768dea2c348691d38af
wavy_ <you.meet.vvavy@gmail.com>
2023-04-21 05:28:38 +0900

    Update 문자열 내 p와 y의 개수.js

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,5 +1,3 @@
 function solution(s){
-    const p = s.match(/p/ig) ? s.match(/p/ig).length : 0
-    const y = s.match(/y/ig) ? s.match(/y/ig).length : 0
-    return p === y ? true : false
+    return s.split(/p/i).length === s.split(/y/i).length ? true : false
 }
