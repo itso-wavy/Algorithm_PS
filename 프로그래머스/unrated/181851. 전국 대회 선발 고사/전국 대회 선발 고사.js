ad919d9d519d9c1fa989957f310359716846fdeb
wavy_ <you.meet.vvavy@gmail.com>
2023-04-21 12:23:14 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -0,0 +1,6 @@
+function solution(rank, attendance) {
+    // const arr = rank.map((v, i) => [v, i])
+    // arr.sort() // [3등, 3번]
+    const arr = rank.map((v, i) => [v, i]).filter((v, i) => attendance[i] === true).sort((a, b) => a[0] - b[0])
+    return 10000 * arr[0][1] + 100 * arr[1][1] + arr[2][1];
+}
