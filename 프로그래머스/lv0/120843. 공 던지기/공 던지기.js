9bd9e2cb46354104387191573cb2f32bb6803994
wavy_ <you.meet.vvavy@gmail.com>
2023-04-26 12:33:33 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,4 +1,3 @@
 function solution(numbers, k) {
-    const index = (k*2-1) % numbers.length - 1
-    return (index < 0 ? numbers[numbers.length+index]: numbers[index])
+    return numbers[(k*2-2) % numbers.length]
 }
