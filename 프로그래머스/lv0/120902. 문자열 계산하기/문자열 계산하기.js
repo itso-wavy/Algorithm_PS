e0245f277ed03048de4e515ebd73d18f2e8c6cae
wavy_ <you.meet.vvavy@gmail.com>
2023-05-10 14:12:25 +0900

    [level 0] Title: 문자열 계산하기, Time: 2.47 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    const arr = my_string.split(' ')
+    let num = 0;
+    for(let i=0; i<arr.length; i+=2){
+        num += (arr[i-1] === '-'? -arr[i] : +arr[i])
+        console.log(i, arr[i-1], arr[i], num)
+    }
+    return num
+}
