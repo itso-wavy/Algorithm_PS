1f89096a98f64de1101dc790661c6a02d29b49c9
wavy_ <you.meet.vvavy@gmail.com>
2023-05-13 05:00:34 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,8 +1,8 @@
 function solution(id_pw, db) {
-    const index = db.map(i => i[0]).indexOf(id_pw[0])
-    return index !== -1
-        ? (db[index][1] === id_pw[1]
-           ? 'login'
-           : 'wrong pw')
+    const map = new Map(db)
+    return map.has(id_pw[0]) 
+        ? map.get(id_pw[0]) === id_pw[1] 
+            ? 'login'
+            : 'wrong pw'
         : 'fail'
 }
