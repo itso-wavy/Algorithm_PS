29db8e33e65ac7b89697dd556ac5ef46bb75b7b9
wavy_ <you.meet.vvavy@gmail.com>
2023-05-13 06:44:16 +0900

    [unrated] Title: 특수문자 출력하기, Time: 41.29 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.js
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.js
@@ -1,7 +1,1 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-}).on('close', function () {
-    console.log(`!@#$%^&*(\\'"<>?:;`)
-});
+console.log(`!@#$%^&*(\\'"<>?:;`)
