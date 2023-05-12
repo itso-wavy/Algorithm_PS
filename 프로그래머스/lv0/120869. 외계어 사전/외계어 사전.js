683484b0c1fc3088d2c4f0666f9b0d4abd4210ad
wavy_ <you.meet.vvavy@gmail.com>
2023-05-12 17:56:39 +0900

    [level 0] Title: 외계어 사전, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,10 +1,3 @@
 function solution(spell, dic) {
-    let i;
-    for(i=0; i<dic.length; i++) {
-        const arr = spell.map(j => {
-            return new RegExp(j).test(dic[i])
-        })
-        if(arr.includes(false)) continue;
-        else return 1;
-    } return 2;
+    return dic.some(i => [...i].sort().join('') === spell.sort().join('')) ? 1 : 2
 }
