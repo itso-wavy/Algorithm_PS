33e67a6de0a2f3923c1da1db744aed51b7b14e4c
wavy_ <you.meet.vvavy@gmail.com>
2023-05-12 19:13:52 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,7 +1,6 @@
 function solution(array) {
-    const set = new Set(array)
-    const arr = []
-    set.forEach((v,i) => arr.push([i, array.filter(j => j === v).length]))
-    const sortArr = arr.sort((a,b) => b[1] - a[1])
-    return sortArr.length === 1 ? sortArr[0][0] : (sortArr[0][1] !== sortArr[1][1] ? sortArr[0][0] : -1)
+    const map = new Map()
+    array.forEach(i => map.set(i, (map.get(i) || 0) + 1))
+    const sorted = [...map].sort((a,b) => b[1] - a[1])
+    return (sorted.length === 1 || sorted[0][1] !== sorted[1][1]) ? sorted[0][0] : -1
 }
