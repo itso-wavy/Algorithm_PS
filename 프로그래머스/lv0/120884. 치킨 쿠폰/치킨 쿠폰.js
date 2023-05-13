8b7de93154a9cbd6de7d760c055eade861275773
wavy_ <you.meet.vvavy@gmail.com>
2023-05-13 20:36:33 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,10 +1,3 @@
 function solution(chicken) {
-    let answer = 0
-    const recur = chicken => {
-        const servis = ~~(chicken/10)
-        const rest = chicken%10
-        answer += servis
-        return servis+rest < 10 ? answer : recur(servis+rest)
-    }
-    return recur(chicken)
+    return ~~((chicken-1) / 9);
 }
