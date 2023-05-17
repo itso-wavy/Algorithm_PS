755e758c3eff58e75d70482e496361a49cb70cb1
wavy_ <you.meet.vvavy@gmail.com>
2023-05-18 04:03:45 +0900

    [level 1] Title: 직사각형 별찍기, Time: 267.54 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,11 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const [n, m] = data.split(" ");
+    let answer = ''
+    for(let i =0; i<m; i++) {
+        for(let j=0; j<n; j++){
+            answer += (j === n-1? '*\n' : '*')
+        }
+    }
+    console.log(answer);
+});
