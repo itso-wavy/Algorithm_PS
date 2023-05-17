b80356f13f28626bfa1ccfee59b7e9ca8e8ba6c5
wavy_ <you.meet.vvavy@gmail.com>
2023-05-18 02:48:44 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,10 +1,5 @@
 function solution(a, d, included) {
-    // 3 7 11 15 ...
-    const arr = []
-    let i = a
-    for(let j = 0; j < included.length; j++) {
-        if (included[j]) arr.push(i)
-        i += d
-    }
-    return arr.reduce((a,c) => a+c, 0);
+    let result = 0
+    included.forEach((v,i) => result = (v ? a+i*d : 0) + result)
+    return result;
 }
