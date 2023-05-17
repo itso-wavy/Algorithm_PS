7f754e0a23b1f9bf411451185927ef400e8bb879
wavy_ <you.meet.vvavy@gmail.com>
2023-05-18 03:38:29 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,8 @@
+function solution(a, b, c) {
+    const set = new Set([a,b,c])
+    switch (set.size) {
+        case 1: return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+        case 2: return (a + b + c) * (a**2 + b**2 + c**2)
+        case 3: return a + b + c
+    }
+}
