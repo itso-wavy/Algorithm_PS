cbae403aab2834ee73efde8d7d59ee385477ce9c
wavy_ <you.meet.vvavy@gmail.com>
2023-05-20 19:46:34 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,23 +1,7 @@
 function solution(n) {
-    // 1) Time: 0.25 ms, Memory: 33.6 MB
-//     const arr = []
-//     for(let i = 1; i <= Math.sqrt(n); i++) {
-//         !(n % i) && (arr.push(i), arr.push(n/i))
-//     }
-//     return arr.sort((a,b) => a-b)
-    
-        let s = new Set();
+    let s = new Set();
     for (let i = 1; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) {
-            s.add(i);
-            s.add(n / i);
-        }
+        !(n % i) && (s.add(i), s.add(n / i))
     }
     return [...s].sort((a, b) => a - b);
-    // 3) Time: 0.24 ms, Memory: 33.5 MB
-    // const set = new Set()
-    // for(let i = 1; i <= n; i++) {
-    //     !(n % i) && set.add(i)
-    // }
-    // return [...set]
 }
