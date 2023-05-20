1fe80eb0c4a896d01c6f62babdfb6dc0e84f63fd
wavy_ <you.meet.vvavy@gmail.com>
2023-05-20 21:18:19 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,8 +1,3 @@
 function solution(n) {
-    const arr = [0, 1]
-    for(let i=2; i<=n; i++){
-        arr[i] = arr[i-1] + 1
-        while(!(arr[i] % 3) || /3/.test(arr[i]+'')) ++arr[i]
-    }
-    return arr[n]
+    return Array.from({length: n*3}, (_,i) => i).filter(i => i%3 && !(/3/.test(i)))[n-1]
 }
