e33fe03dc6c3c7b0f5fcd8886c87f71b124f8355
wavy_ <you.meet.vvavy@gmail.com>
2023-05-21 09:56:00 +0900

    [level 2] Title: 올바른 괄호, Time: 2.52 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,10 +1,8 @@
 function solution(s){
-    if(s[0]==='(' && s.slice(-1) === ')') {
-        let acc = 0
-        for(let i=0; i<s.length; i++) {
-            s[i] === '(' ? acc++ : acc--
-            if(acc < 0) return false
-        } 
-        return acc === 0 ? true : false
-    } return false
+    let acc = 0
+    for(let i=0; i<s.length; i++) {
+        s[i] === '(' ? acc+=1 : acc-=1
+        if(acc < 0) return false
+    } 
+    return acc === 0 ? true : false
 }
