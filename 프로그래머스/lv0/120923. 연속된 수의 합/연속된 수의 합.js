488593b731dadeb6c0d669794f8d5304935f73dd
wavy_ <you.meet.vvavy@gmail.com>
2023-05-23 07:42:17 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,5 +1,3 @@
 function solution(num, total) {
-    const arr = []
-    for(let i=Math.ceil(total/num) - ~~(num/2); arr.length<num; i++) arr.push(i)
-    return arr
+    return Array.from({length: num}, (v,i) => Math.ceil(total/num) - ~~(num/2) + i)
 }
