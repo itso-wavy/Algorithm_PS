c201b258456a2203af7c0067a07981c29e51a133
wavy_ <you.meet.vvavy@gmail.com>
2023-05-25 08:21:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,6 +1,6 @@
 function solution(s) {
-    const obj = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+    const arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-    for(let key in obj) s = s.replaceAll(obj[key], key)
+    for(let i in arr) s = s.replaceAll(arr[i], i)
     return s*1
 }
