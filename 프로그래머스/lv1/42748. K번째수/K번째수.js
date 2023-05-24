0289d9a01ae9ebba6b3043a9dce308da02d48b44
wavy_ <you.meet.vvavy@gmail.com>
2023-05-24 14:59:01 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,6 +1,5 @@
 function solution(array, commands) {
-    return commands.map(v => {
-        const [i, j, k] = v
+    return commands.map(([i, j, k]) => {
         return array.slice(i-1,j).sort((a,b) => a-b)[k-1]
     })
 }
