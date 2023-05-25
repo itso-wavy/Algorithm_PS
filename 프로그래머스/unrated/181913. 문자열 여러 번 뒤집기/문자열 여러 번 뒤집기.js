34edc9e88d857604b36838bc179bf5eafade3ad9
wavy_ <you.meet.vvavy@gmail.com>
2023-05-26 01:23:19 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 5.13 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,6 +1,6 @@
 function solution(my_string, queries) {
     const arr = [...my_string]
-    queries.forEach(([s,e],i) => {
+    queries.forEach(([s,e]) => {
         for(let i=0; i<(e-s)/2; i++) {
             [arr[s+i], arr[e-i]] = [arr[e-i], arr[s+i]]
         } 
