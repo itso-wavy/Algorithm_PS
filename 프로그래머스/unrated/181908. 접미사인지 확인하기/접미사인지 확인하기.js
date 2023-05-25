158206d4806085f36502a19cb5e0fd4f71f72ecc
wavy_ <you.meet.vvavy@gmail.com>
2023-05-25 16:40:37 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,5 +1,3 @@
 function solution(my_string, is_suffix) {
-    my_string = [...my_string].reverse().join('')
-    is_suffix = [...is_suffix].reverse().join('')
-    return +my_string.startsWith(is_suffix)
+    return +my_string.endsWith(is_suffix)
 }
